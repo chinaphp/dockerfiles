b941e61e1c74dd92a923af598ae3b8d6d7ceb77d
jack <264110@qq.com>
2020-02-17 02:27:24 +0800

    s6 v1.22.1.0

---
+++ update_s6_overlay.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+#set -x
+
+BASE=$(readlink -f $(dirname $0))
+VERSION=$1
+
+if [ -z ${VERSION} ]; then
+  echo "Please run with version number! Ex. ${0} v1.22.1.0"
+  exit 1
+fi
+
+cd ${BASE}
+
+for file in */Dockerfile; do
+	DIR=$(dirname ${file})
+	IMAGE=$(basename ${DIR})
+
+	sed -i "s#\(https://github.com/just-containers/s6-overlay/releases/download\)/[^/]*/\(s6-overlay-amd64.tar.gz\)#\1/${VERSION}/\2#g" ${file}
+
+	#break
+done
